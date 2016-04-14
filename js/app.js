3093b5d380740762c5378695e4651193affbb97d
Zhelyazko Stoyanov <zh.stoqnov@gmail.com>
2016-04-14 21:23:27 +0300

    added ui.router angular lib

--- js/app.js
+++ js/app.js
@@ -11,11 +11,26 @@ app.constant('BASE_URL', 'http://softuni-issue-tracker.azurewebsites.net/api/');
 
 app.config(function ($stateProvider) {
     $stateProvider.state('root', {
+        url: '',
+        controller: function ($state) {
+            if($state.is('root')) {
+                $state.go(sessionStorage['currentUser'] ? "dashboard" : "anonymous");
+            }
+        }
+    });
+
+    $stateProvider.state('anonymous', {
         templateUrl: 'templates/home.html',
         controller: 'HomeController'
     });
+
+    $stateProvider.state('dashboard', {
+        templateUrl: 'templates/dashboard.html',
+        controller: 'DashboardController'
+    });
 });
 
+// TODO Delete code below
 /*
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
