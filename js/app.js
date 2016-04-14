6202f798a0e68820a60c67442ab1665fddf04ee3
Zhelyazko Stoyanov <zh.stoqnov@gmail.com>
2016-04-14 21:14:24 +0300

    implemented additional authentication functions

--- js/app.js
+++ js/app.js
@@ -2,12 +2,21 @@
 
 var app = angular.module('issueSystem', [
     'ngRoute',
+    'ui.router',
     'issueSystem.home',
     'issueSystem.users.identity'
     ]);
 
 app.constant('BASE_URL', 'http://softuni-issue-tracker.azurewebsites.net/api/');
 
+app.config(function ($stateProvider) {
+    $stateProvider.state('root', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+    });
+});
+
+/*
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'templates/home.html',
@@ -15,4 +24,4 @@ app.config(['$routeProvider', function ($routeProvider) {
     });
 
     $routeProvider.otherwise({redirectTo: '/'});
-}]);
+}]);*/
--- js/services/authService.js
+++ js/services/authService.js
@@ -50,28 +50,79 @@ angular.module('issueSystem.users.authentication', [])
             }
 
             function logout() {
-
+                delete sessionStorage['currentUser'];
             }
 
             function isLoggedIn() {
+                return sessionStorage['currentUser'] != undefined;
+            }
 
+            function isAnonymous() {
+                return sessionStorage['currentUser'] == undefined;
             }
 
             function isNormalUser() {
-
+                this.getUserInfo()
+                    .then(function (userInfo) {
+                        return !userInfo.isAdmin;
+                    });
             }
 
             function isAdmin() {
+                this.getUserInfo()
+                    .then(function (userInfo) {
+                        return userInfo.isAdmin;
+                    });
+            }
 
+            function getCurrentUser() {
+                var user = sessionStorage['currentUser'];
+                if(user) {
+                    return JSON.parse(sessionStorage['currentUser']);
+                }
             }
 
             function getAuthHeaders() {
+                var headers = {};
+                var user = this.getCurrentUser();
+                if(user) {
+                    headers['Authorization'] = 'Bearer ' + user.access_token;
+                }
+            }
 
+            function getUserInfo() {
+                var bearerToken = this.getAuthHeaders();
+
+                var request = {
+                    method: 'GET',
+                    url: BASE_URL + 'users/me',
+                    headers: {
+                        'Authorization': bearerToken
+                    }
+                };
+
+                var deferred = $q.defer();
+
+                $http(request)
+                    .then(function (response) {
+                        deferred.resolve(response.data);
+                    }, function (error) {
+                        deferred.reject(error.data);
+                    });
+
+                return deferred.promise;
             }
 
             return {
                 registerUser: registerUser,
                 loginUser: loginUser,
-                logout: logout
+                logout: logout,
+                isLoggedIn: isLoggedIn,
+                isAnonymous: isAnonymous,
+                isNormalUser: isNormalUser,
+                isAdmin: isAdmin,
+                getCurrentUser: getCurrentUser,
+                getAuthHeaders: getAuthHeaders,
+                getUserInfo: getUserInfo
             }
     }]);
