6be22dedc1b649b1891787e986644eed4eb070ec
Zhelyazko Stoyanov <zh.stoqnov@gmail.com>
2016-04-29 20:10:13 +0300

    jquery ui added to project

--- js/app.js
+++ js/app.js
@@ -14,7 +14,8 @@ var app = angular.module('issueSystem', [
     'issueSystem.projects',
     'issueSystem.users.usersService',
     'issueSystem.issues',
-    'issueSystem.issues.issuesService'
+    'issueSystem.issues.issuesService',
+    'issueSystem.common.datepicker'
     ]);
 
 app.constant('BASE_URL_API', 'http://softuni-issue-tracker.azurewebsites.net/api/');
